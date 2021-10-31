d923789f1325ade5f598aa9b3b68d7452831dcd8
noraSSH <nora.sun@mail.utoronto.ca>
2021-10-31 12:19:10 -0400

    add search and login require

--- tests/app_test.py
+++ tests/app_test.py
@@ -7,18 +7,6 @@ from project.app import app, db
 
 TEST_DB = "test.db"
 
-
-@pytest.fixture
-def client():
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    app.config["TESTING"] = True
-    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
-
-    init_db() # setup
-    yield app.test_client() # tests run here
-    init_db() # teardown
-
-
 def login(client, username, password):
     """Login helper function"""
     return client.post(
@@ -31,7 +19,6 @@ def login(client, username, password):
 def logout(client):
     """Logout helper function"""
     return client.get("/logout", follow_redirects=True)
-
 
 def test_index(client):
     response = client.get("/", content_type="html/text")
@@ -76,9 +63,24 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_search(client):
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    entry = client.post(
+        "/add",
+        data=dict(title="<A>", text="<strong>HTML</strong> 2"),
+        follow_redirects=True,
+    )
+    rv = client.post("/search/", data="<A>", follow_redirects=True)
+    assert b"&lt;A&gt;" not in rv.data
+    assert b"<strong>HTML</strong> 2" not in rv.data
 
 @pytest.fixture
 def client():
